85c85bad8e818989ee57aba405c279a60060ae17
hhofner <hhofner@protonmail.com>
2018-10-06 15:16:26 +0900

    change name, remove file

--- get_market_vals.py
+++ get_market_vals.py
@@ -16,7 +16,7 @@ import pandas as pd
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
 def convertValueStringToInteger(value_string):
-    ''' Convert a web-scraped string of a value into integer 
+    ''' Convert a web-scraped string of a value into integer
         Example: 3,50 Mio. € ==> 3500000
     '''
     if len(value_string) < 2:
@@ -27,27 +27,27 @@ def convertValueStringToInteger(value_string):
             exp = multipliers[value_string[1]]
         except KeyError:
             raise Exception('Unknown exponent: %s' % value_string[1])
-            
+
         value = float(value_string[0].replace(',','.')) * exp
         return value
 
 def getPlayerValues(page, headers):
     pageTree = requests.get(page, headers=headers)
     pageSoup = BeautifulSoup(pageTree.content, 'html.parser')
-    
+
     Players = pageSoup.find_all("a", {"class":"spielprofil_tooltip"})
-    
+
     Values = pageSoup.find_all("td", {"class":"rechts hauptlink"})
-    
+
     PlayersList = []
     ValuesList = []
 
     # TODO: assert len(Players) == len(Values), 'Non-matching Players and Values sizes'
-    
+
 #    for i in range(len(Players)):
 #        PlayersList.append(Players[i].text)
 #        ValuesList.append(Values[i].text)
-    
+
     i=0;j=0
     while(j<len(Values)):
         PlayersList.append(Players[i].text)
@@ -55,10 +55,10 @@ def getPlayerValues(page, headers):
         value = convertValueStringToInteger(value_string)
         ValuesList.append(value)
         i += 2; j += 1
-        
+
     # Create Pandas dataframe
     df = pd.DataFrame({"Players":PlayersList, "Values":ValuesList})
-    
+
     return df
 
 
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     cardiff_city_url = 'https://www.transfermarkt.de/cardiff-city/kader/verein/603'
     cardiffcity_df   = getPlayerValues(cardiff_city_url, headers)
     print(cardiffcity_df.head())
-    
+
     # Man City
     man_city_url = 'https://www.transfermarkt.de/manchester-city/kader/verein/281'
     mancity_df = getPlayerValues(man_city_url, headers)
